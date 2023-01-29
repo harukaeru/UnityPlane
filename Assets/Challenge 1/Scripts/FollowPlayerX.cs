39624e97060efe1a89cfd0a8d1f7066b0272404f
harukaeru <nekoneko.myaomyao@gmail.com>
2023-01-30 05:21:29 +0900

    Plane

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+  public GameObject plane;
+  public Vector3 offset = new Vector3(0, 1.5f, -10);
+  private float distance;
+  private Vector3 playerPrevPos;
+  private Vector3 playerMoveDir;
+  private float speed = 30.0f;
+
+  private float inversedDegree = 180;
+
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    distance = offset.magnitude;
+    playerPrevPos = plane.transform.position;
+  }
+
+  void FixedUpdate()
+  {
+    var rot = Quaternion.LookRotation(plane.transform.position - transform.position);
+    transform.rotation = Quaternion.Slerp(transform.rotation, rot, speed * Time.deltaTime);
+
+    // Move
+    Vector3 newPosition = plane.transform.position + plane.transform.forward * offset.z + plane.transform.up * offset.y;
+    transform.position = Vector3.Slerp(transform.position, newPosition, Time.deltaTime * speed);
+  }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+  public float speed;
+  public float rotationSpeed;
+  public float verticalInput;
+
+  private float accelerator = 0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void FixedUpdate()
+  {
+    // get the user's vertical input
+    verticalInput = Input.GetAxis("Vertical");
+    var a = Input.GetKey("a");
+    var z = Input.GetKey("z");
+    if (a)
+    {
+      accelerator = Mathf.Min(10000, accelerator + 1);
+    }
+    else if (z)
+    {
+      accelerator = Mathf.Max(0, accelerator - 1);
+    }
+
+    // move the plane forward at a constant rate
+    var v = Vector3.forward * speed * accelerator;
+    Debug.Log("v:" + v);
+    transform.Translate(Vector3.forward * speed * accelerator);
+
+    // tilt the plane up/down based on up/down arrow keys
+    transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+  }
+}
